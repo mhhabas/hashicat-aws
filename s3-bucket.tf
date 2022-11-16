884d029aeb8fbcf5700af76f371f94ae3bf6e895
Dirk Stagger <mark.habas@gmail.com>
2022-11-16 22:10:18 +0000

    Added s3 bucket module 2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source        = "app.terraform.io/fiddycorp-tutorials/s3-bucket/aws"
-  version       = "2.8.0"
+  source  = "app.terraform.io/fiddycorp-tutorials/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = var.prefix
 }
