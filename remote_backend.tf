7ff75e9009070801100aa461e678f79e3f752181
Dirk Stagger <mark.habas@gmail.com>
2022-11-16 22:05:22 +0000

    Added s3 bucket module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "fiddycorp-tutorials"
     workspaces {
       name = "hashicat-aws"
---
+++ s3-bucket.tf
@@ -0,0 +1,5 @@
+module "s3-bucket" {
+  source        = "app.terraform.io/fiddycorp-tutorials/s3-bucket/aws"
+  version       = "2.8.0"
+  bucket_prefix = var.prefix
+}
